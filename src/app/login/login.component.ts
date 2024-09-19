1ed8264ef3d3b5d12805be16f9120507a16adc21
unknown <j.quabeck@gmx.net>
2024-09-19 21:50:03 +0200

    design login-Page roughly implemented

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
