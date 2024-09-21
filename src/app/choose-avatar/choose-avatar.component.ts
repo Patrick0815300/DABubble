8f2352eed3ab610bda1c90012a3c83b8b42848bd
unknown <j.quabeck@gmx.net>
2024-09-21 13:52:21 +0200

    2 bug fixes

--- src/app/choose-avatar/choose-avatar.component.ts
+++ src/app/choose-avatar/choose-avatar.component.ts
@@ -29,7 +29,7 @@ export class ChooseAvatarComponent {
 
   avatar:string[] = ["Elias_Neumann","Elise_Roth","Frederik_Beck","Noah_Braun","Sofia_Müller","Steffen_Hoffmann"];
 
-  Userregistrated: boolean = true;
+  Userregistrated: boolean = false;
 
   registerCompleted(){
     this.Userregistrated = true;
