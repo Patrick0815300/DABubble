b32a3d77f6948b62f093c21e0c321c6eb5a66ac6
unknown <j.quabeck@gmx.net>
2024-10-10 18:19:06 +0200

    added imprint and privacy policy

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ImprintComponent } from '../imprint/imprint.component';
+import { RouterModule } from '@angular/router';
+import { PrivacypolicyComponent } from '../privacypolicy/privacypolicy.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [ImprintComponent, PrivacypolicyComponent, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
---
+++ src/app/imprint/imprint.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-imprint',
+  standalone: true,
+  imports: [MatCardModule, RouterModule],
+  templateUrl: './imprint.component.html',
+  styleUrl: './imprint.component.scss'
+})
+export class ImprintComponent {
+
+}
